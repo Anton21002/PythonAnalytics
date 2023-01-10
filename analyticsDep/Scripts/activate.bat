164e9e752643f4d0cc803aff6e80ecbc23c12521
Anton21002 <anton.satalin@student.hamk.fi>
2023-01-10 12:31:57 +0200

    Reinstalled libaries

--- analyticsDep/Scripts/activate.bat
+++ analyticsDep/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\student\Documents\GitHub\AnalysisGroup14\analyticsDep
+set VIRTUAL_ENV=C:\Users\Anton\Documents\GitHub\AnalysisGroup14\analyticsDep
 
 if not defined PROMPT set PROMPT=$P$G
 
